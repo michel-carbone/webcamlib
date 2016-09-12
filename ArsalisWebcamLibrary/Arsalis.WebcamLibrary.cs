71d7ef4573a6a736bd73af1d21140e93f6293d51
arsalis <arsalis@8a958d7f-5ac1-4ef9-84b4-c6d7a612697d>
2016-09-12 11:55:50 +0000

    first version that list available webcams

---
+++ ArsalisWebcamLibrary/Arsalis.WebcamLibrary.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: michel
+ * Date: 9/09/2016
+ * Time: 17:29
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Controls;
+using AForge.Math;
+
+namespace Arsalis.WebcamLibrary
+{
+	/// <summary>
+	/// Description of Arsalis_WebcamLibrary.
+	/// </summary>
+	public class WebcamLibrary
+	{
+		public WebcamLibrary()
+		{
+			loadWebcamDevices();
+		} 
+		
+		public string [] WebcamListNames = new string[10];
+		// collection of available video devices
+        private FilterInfoCollection videoDevices;
+		
+		private void loadWebcamDevices()
+		{
+			try
+			{
+                // enumerate video devices
+                this.videoDevices = new FilterInfoCollection( FilterCategory.VideoInputDevice );
+
+                if ( this.videoDevices.Count == 0 )
+                    throw new ApplicationException( );
+
+                // add all devices to combo
+                int i = 0;
+                foreach ( FilterInfo device in this.videoDevices )
+                {
+                	WebcamListNames[i] = device.Name +"\r\n"+device.MonikerString;
+                	
+                	i=++i;
+                }
+            }
+            catch ( ApplicationException )
+            {
+                //TODO manage exception
+            }
+		}
+	}
+}
---
+++ ArsalisWebcamLibraryTest/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: michel
+ * Date: 9/09/2016
+ * Time: 17:40
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Arsalis.WebcamLibrary.Test
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			Arsalis.WebcamLibrary.WebcamLibrary test = new WebcamLibrary();
+			for(int i = 0; i<test.WebcamListNames.Length; i++)
+			{
+				Console.WriteLine(i.ToString() + ": " + test.WebcamListNames[i]);
+			}
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
