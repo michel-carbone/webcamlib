087741994caddb33d6d84e4e0aa84a985fcf0dd7
arsalis <arsalis@8a958d7f-5ac1-4ef9-84b4-c6d7a612697d>
2016-12-01 16:50:51 +0000

    enable visual style and compile to console instaed of to win app

--- WebcamSetupGUI/Program.cs
+++ WebcamSetupGUI/Program.cs
@@ -14,10 +14,11 @@ namespace WebcamSetupGUI
         public static void Main(string[] args)
         {
         	System.Console.WriteLine("Start WebcamSetupGUI");
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Arsalis.WebcamLibrary.WebcamSetupGUI());
             Arsalis.WebcamLibrary.WebcamSetupGUI gui = new Arsalis.WebcamLibrary.WebcamSetupGUI();
+            
             gui.ShowDialog();
             System.Console.WriteLine("Exit WebcamSetupGUI");
         }
